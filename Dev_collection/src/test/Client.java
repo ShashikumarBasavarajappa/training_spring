731467034dbf8ca3d3891a77d1d0c403a5bf5219
Basavarajappa <insbasavar@ad.harman.com>
2017-02-04 21:54:16 +0530

    Signed-off-by: Basavarajappa <insbasavar@ad.harman.com>

---
+++ Dev_collection/src/test/Client.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import beans.Test;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext app = new ClassPathXmlApplicationContext("resources/spring.xml");
+		
+		Test tt =(Test)app.getBean("t");
+		tt.printdata();
+	}
+}
